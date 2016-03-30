f1c8f15ba4a51b88456ab24f1e0b0c52c0bdf565
James Duerden <jamie.f.duerden@gmail.com>
2016-03-31 00:15:55 +0100

    Contact form

--- content/project/sloggit.md
+++ content/project/sloggit.md
@@ -2,3 +2,5 @@
 date = "2016-03-30T16:18:26+01:00"
 title = "Sloggit"
 +++
+
+[<i class="fa fa-github fa-3x"></i>](https://github.com/Xymist/sloggit)
---
+++ static/js/contact.js
@@ -0,0 +1,12 @@
+function encrypt_if_possible() {
+  //var openpgp = require('openpgp');
+  //var key = document.getElementById('pub-key').innerHTML;
+  //var publicKey = openpgp.key.readArmored(key);
+  //openpgp.encryptMessage(publicKey.keys, document.getElementById('main-input').value).then(function(pgpMessage) {
+  //  $('#main-input').value=pgpMessage;
+  //  $("contact-form").submit();
+  //}).catch(function(error) {
+  //  console.log("Failed to encrypt message; sending in plaintext");
+    $("contact-form").submit();
+  //});
+}
