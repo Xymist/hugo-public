770f8c77c691cac65e166a8abad1c9fdf0852a81
James Duerden <jamie.f.duerden@gmail.com>
2016-05-19 22:24:05 +0100

    Contact form improved?

--- static/js/contact.js
+++ static/js/contact.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $('#contact-form').submit(function (event) {
     $.ajax({
       type     : 'POST',
-      url      : 'https://jamieduerden.co.uk/pages/contact',
+      url      : 'https://jamieduerden.co.uk/mailout',
       data     : $('#contact-form').serialize(),
       dataType : 'json',
       encode   : true
